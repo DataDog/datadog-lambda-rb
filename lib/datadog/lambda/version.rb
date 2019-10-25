db3e19a19d39acfdbfc6838d68c007c030fc0c31
DarcyRaynerDD <darcy.rayner@datadoghq.com>
2019-10-25 11:06:49 -0400

    Bump patch number

--- lib/datadog/lambda/version.rb
+++ lib/datadog/lambda/version.rb
@@ -13,7 +13,7 @@ module Datadog
     module VERSION
       MAJOR = 0
       MINOR = 3
-      PATCH = 0
+      PATCH = 1
       PRE = nil
 
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
