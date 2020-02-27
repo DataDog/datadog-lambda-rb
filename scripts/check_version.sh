243ed85cfac929a0dce1c4d35cb8e98cdc1f793b
chris.agocs <chris.agocs@datadoghq.com>
2020-02-27 15:28:25 -0500

    Add version checks

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -11,19 +11,36 @@
 
 set -e
 
+LAYER_NAME=Datadog-Ruby2-5
+PROD_ACCOUNT=464622532012
+AVAILABLE_REGIONS=(us-east-2 us-east-1 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-north-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1)
+
+list_layers () {
+	outfile=$1
+	for region in "${AVAILABLE_REGIONS[@]}"
+	do
+		thisArn=arn:aws:lambda:$region:$PROD_ACCOUNT:layer:$LAYER_NAME
+		last_layer_arn=$(aws lambda list-layer-versions --layer-name $thisArn --region $region | jq -r ".LayerVersions | .[0] |  .Version")
+		if [ -z $last_layer_arn ]; then
+			>&2 echo "Unable to list layers for " $thisArn
+			exit 1
+		fi
+		echo $last_layer_arn >> $outfile
+	done
+}
+
 NEW_PACKAGE_VERSION=$(gem build datadog-lambda | grep Version | sed -n -e 's/^.*Version: //p' | perl -n -e'/\.(\d)\./ && print $1')
 
 LAYERLIST=$(mktemp)
-./scripts/list_layers.sh | grep arn > $LAYERLIST
+list_layers $LAYERLIST
 
-DISCRETE_LAYERS=$(cat $LAYERLIST | cut -d ':' -f 8 | sort | uniq | wc -l)
-
+DISCRETE_LAYERS=$(cat $LAYERLIST |  sort | uniq | wc -l)
 if [ $DISCRETE_LAYERS != 1 ]; then
-	>&2 echo "Unexpected number of discrete deployed layer versions found: " $DISCRETE_LAYERS
-	exit 1
+       >&2 echo "Unexpected number of discrete deployed layer versions found: " $DISCRETE_LAYERS
+       exit 1
 fi
 
-LATEST_LAYER_NUMBER=$(cat $LAYERLIST | cut -d ':' -f 8 | head -n 1)
+LATEST_LAYER_NUMBER=$(cat $LAYERLIST | head -n 1)
 
 LATEST_GEM_VERSION=$(gem list datadog-lambda --remote | perl -n -e'/\.(\d)\./ && print $1')
 
