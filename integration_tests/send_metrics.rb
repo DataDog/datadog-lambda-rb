720362bf99cd131269c6aed49de2fecf20780f6a
hghotra <ghotra.harvinder@gmail.com>
2020-08-06 09:15:11 -0400

    Fix issue with send_metrics

--- integration_tests/send_metrics.rb
+++ integration_tests/send_metrics.rb
@@ -29,26 +29,26 @@ def handle(event:, context:)
             eventsource: response_payload['eventType']
           )
         end
-        if record['Sns']
-          response_payload['eventType'] = 'SNS'
-          response_payload['recordIds'].push(record['Sns']['MessageId'])
-          Datadog::Lambda.metric(
-            'serverless.integration_test.records_processed',
-            1,
-            tagkey: 'tagvalue',
-            eventsource: response_payload['eventType']
-          )
-        end
+        next unless record['Sns']
+
+        response_payload['eventType'] = 'SNS'
+        response_payload['recordIds'].push(record['Sns']['MessageId'])
         Datadog::Lambda.metric(
-          'serverless.integration_test.execution',
+          'serverless.integration_test.records_processed',
           1,
           tagkey: 'tagvalue',
           eventsource: response_payload['eventType']
         )
-
-        puts "Processed #{response_payload['eventType']} request"
       end
     end
+    Datadog::Lambda.metric(
+      'serverless.integration_test.execution',
+      1,
+      tagkey: 'tagvalue',
+      eventsource: response_payload['eventType']
+    )
+
+    puts "Processed #{response_payload['eventType']} request"
     response_payload
   end
 end
