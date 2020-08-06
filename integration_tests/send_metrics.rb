89673f8da11124a6e7676669021754a80258c247
hghotra <ghotra.harvinder@gmail.com>
2020-08-05 21:14:46 -0400

    Integration tests for ruby layer

---
+++ integration_tests/send_metrics.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'datadog/lambda'
+
+Datadog::Lambda.configure_apm do |c|
+end
+
+def handle(event:, context:)
+  Datadog::Lambda.wrap(event, context) do
+    response_payload = {
+      'message' => 'hello, dog!'
+    }
+
+    if event.key?('requestContext')
+      response_payload['eventType'] = 'APIGateway'
+      response_payload['requestId'] = event['requestContext']['requestId']
+    end
+
+    if event.key?('Records')
+      response_payload['recordIds'] = []
+      event['Records'].each do |record|
+        if record['messageId']
+          response_payload['eventType'] = 'SQS'
+          response_payload['recordIds'].push(record['messageId'])
+          Datadog::Lambda.metric(
+            'serverless.integration_test.records_processed',
+            1,
+            tagkey: 'tagvalue',
+            eventsource: response_payload['eventType']
+          )
+        end
+        if record['Sns']
+          response_payload['eventType'] = 'SNS'
+          response_payload['recordIds'].push(record['Sns']['MessageId'])
+          Datadog::Lambda.metric(
+            'serverless.integration_test.records_processed',
+            1,
+            tagkey: 'tagvalue',
+            eventsource: response_payload['eventType']
+          )
+        end
+        Datadog::Lambda.metric(
+          'serverless.integration_test.execution',
+          1,
+          tagkey: 'tagvalue',
+          eventsource: response_payload['eventType']
+        )
+
+        puts "Processed #{response_payload['eventType']} request"
+      end
+    end
+    response_payload
+  end
+end
