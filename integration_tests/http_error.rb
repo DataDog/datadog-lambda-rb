cac1223ba61361fe72af7fab2ea890d598f8a12d
Sergio Prada <chechopradaster@gmail.com>
2020-08-20 15:27:32 -0400

    add another case for integration tests

---
+++ integration_tests/http_error.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'datadog/lambda'
+require 'net/http'
+
+Datadog::Lambda.configure_apm do |c|
+end
+
+def handle(event:, context:)
+  Datadog::Lambda.wrap(event, context) do
+    response_payload = {
+      'message' => 'hello, dog!'
+    }
+    Datadog::Lambda.metric('serverless.integration_test.execution', 1, function: 'http-request')
+
+    Net::HTTP.get_response('httpstat.us', '/400')
+
+    puts("Snapshot test http requests successfully made to URLs: #{urls}")
+    response_payload
+  end
+end
