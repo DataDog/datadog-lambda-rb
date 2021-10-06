60700c7b80fa70a4dc8d9588a9f83046188a1371
Ivan Topolcic <ivan.topolcic@datadoghq.com>
2021-10-06 14:39:42 -0400

    Change LAYER_VERSION to VERSION

--- scripts/publish_sandbox.sh
+++ scripts/publish_sandbox.sh
@@ -5,7 +5,7 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019 Datadog, Inc
 
-# Usage: LAYER_VERSION=5 ./scripts/publish_sandbox.sh
+# Usage: VERSION=5 ./scripts/publish_sandbox.sh
 
 set -e
 
