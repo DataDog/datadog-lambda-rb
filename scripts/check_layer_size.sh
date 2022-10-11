aa3e2a55deb1e4e7f4a7e1c192ea59997979651a
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2022-10-11 15:41:57 -0400

    update size limits

--- scripts/check_layer_size.sh
+++ scripts/check_layer_size.sh
@@ -8,8 +8,8 @@
 # Compares layer size to threshold, and fails if below that threshold
 
 # 5 mb size limit
-MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 8 \* 1024) 
-MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 21 \* 1024) 
+MAX_LAYER_COMPRESSED_SIZE_KB=$(expr 28 \* 1024) 
+MAX_LAYER_UNCOMPRESSED_SIZE_KB=$(expr 80 \* 1024) 
 
 
 LAYER_FILES_PREFIX="datadog-lambda_ruby"
