1f6eed97a44f13cc8a093057a2b87ae6c079c333
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-06-22 17:04:00 -0400

    add `DD_SPAN_ID_HEADER`

--- lib/datadog/lambda/trace/constants.rb
+++ lib/datadog/lambda/trace/constants.rb
@@ -16,6 +16,7 @@ module Datadog
     SAMPLE_MODE_USER_KEEP = 2
     DD_TRACE_ID_HEADER = 'x-datadog-trace-id'
     DD_PARENT_ID_HEADER = 'x-datadog-parent-id'
+    DD_SPAN_ID_HEADER = 'x-datadog-span-id'
     DD_SAMPLING_PRIORITY_HEADER = 'x-datadog-sampling-priority'
     DD_XRAY_SUBSEGMENT_NAME = 'datadog-metadata'
     DD_XRAY_SUBSEGMENT_KEY = 'trace'
