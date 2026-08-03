9ac690992b13b4b8c0e0703922d431edd42ef39c
Sergey Fedorov <sergey.fedorov@datadoghq.com>
2026-08-03 11:17:58 +0200

    Extract tagging for AppSec

--- lib/datadog/lambda/appsec.rb
+++ lib/datadog/lambda/appsec.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-require 'json'
-
-require 'datadog/tracing/client_ip'
-require 'datadog/core/header_collection'
-require 'datadog/appsec/default_header_tags'
 require 'datadog/appsec/api_security'
 
+require_relative 'appsec/tagging'
 require_relative 'appsec/request'
 require_relative 'appsec/response'
 require_relative 'appsec/event_normalizer'
@@ -25,12 +21,12 @@ module Datadog
           context = create_context(trace, span)
           return unless Datadog::AppSec::Context.active
 
-          tag_and_keep(context, cold_start: cold_start)
+          Tagging.tag_and_keep(context, cold_start: cold_start)
 
           event = EventNormalizer.normalize(event)
           @request = Request.from_normalized(event)
 
-          add_request_tags(context, @request)
+          Tagging.tag_request(@request, context: context)
           payload = Datadog::AppSec::Instrumentation::Gateway::DataContainer.new(
             event, context: context
           )
@@ -62,7 +58,7 @@ module Datadog
 
           interrupt_params = nil
           unless context.interrupted?
-            add_response_tags(context, response)
+            Tagging.tag_response(response, context: context)
             payload = Datadog::AppSec::Instrumentation::Gateway::DataContainer.new(
               normalized, context: context
             )
@@ -129,61 +125,6 @@ module Datadog
           Datadog::AppSec::Context.activate(context)
 
           context
-        end
-
-        def add_request_tags(context, request)
-          span = context.span
-          return unless span
-
-          headers = Datadog::Core::HeaderCollection.from_hash(request.headers)
-          Datadog::AppSec::DefaultHeaderTags.tag_request(span, headers)
-
-          return if span.get_tag(Datadog::Tracing::Metadata::Ext::HTTP::TAG_CLIENT_IP)
-
-          Datadog::Tracing::ClientIp.set_client_ip_tag!(
-            span, headers: headers, remote_ip: request.remote_addr
-          )
-        end
-
-        def add_response_tags(context, response)
-          span = context.span
-          return unless span
-
-          headers = Datadog::Core::HeaderCollection.from_hash(response.headers)
-          Datadog::AppSec::DefaultHeaderTags.tag_response(span, headers)
-
-          return if headers.get('content-length')
-
-          if (body = response.body)
-            span.set_tag('http.response.headers.content-length', body.bytesize.to_s)
-          end
-        end
-
-        def tag_and_keep(context, cold_start:)
-          span = context.span
-          trace = context.trace
-
-          return unless trace && span
-
-          span.set_metric(Datadog::AppSec::Ext::TAG_APPSEC_ENABLED, 1)
-          span.set_tag('_dd.runtime_family', 'ruby')
-          span.set_tag('_dd.appsec.waf.version', Datadog::AppSec::WAF::VERSION::BASE_STRING)
-
-          ruleset_version = context.waf_runner_ruleset_version
-          return unless ruleset_version
-
-          span.set_tag('_dd.appsec.event_rules.version', ruleset_version)
-
-          return unless cold_start
-
-          trace.keep!
-          trace.set_tag(
-            Datadog::Tracing::Metadata::Ext::Distributed::TAG_DECISION_MAKER,
-            Datadog::Tracing::Sampling::Ext::Decision::ASM
-          )
-          span.set_tag(
-            '_dd.appsec.event_rules.addresses', JSON.dump(context.waf_runner_known_addresses)
-          )
         end
 
         def response_override(interrupt_params, headers:)
